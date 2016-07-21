c0e22516469ac5cb9974b72355ad63d25bf717b7
Pierre Roudier <pierre.roudier@gmail.com>
2016-07-21 13:43:42 +1200

    Added examples section

--- R/spemd.r
+++ R/spemd.r
@@ -25,6 +25,25 @@
 #'
 #' @return .
 #'
+#' @examples
+#'
+#' # Getting sample data from the gstat package
+#' if (require(gstat)) {
+#' library(sp)
+#'
+#' # Example for gridded data
+#' data(ncp.grid, package = 'gstat')
+#' coordinates(ncp.grid) <- ~x+y
+#' gridded(ncp.grid) <- TRUE
+#' res.ncp <- spEMD(ncp.grid, zcol = "depth", thresh.extrema = 0.1, verbose = FALSE)
+#'
+#' # Plot results
+#' spplot(res.ncp[, c('imf1', "imf2", "imf3")])
+#' }
+#'
+#' #
+#'
+#'
 #' @include create_neig.r extrema_irr.r extract_extrema.r mean_enveloppe.r
 #'
 #' @import sp
